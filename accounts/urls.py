81cd440eac839ee6eb7b53d0267330006cfa5c1d
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-02-27 13:38:45 +0000

    start

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from accounts import views
-urlpatterns = [
-    path('login', views.loginuser, name="login"),
-    path('register', views.SignupUser, name="register"),
-    path('logout', views.logoutuser, name="logout"),
-
-]
