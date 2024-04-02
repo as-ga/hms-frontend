19ff9a26bbf42b223e328511a833e9f1aea1b0e8
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-04-02 19:36:51 +0000

    Update code formatting and fix minor bugs

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Hospital Management System (HMS)",
+  description: "Hospital Management System (HMS) is a web application that helps hospitals manage their patients, doctors, and appointments.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
